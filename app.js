7f1ed54c6c4b51d7d3192f2ba45e936e0e4bbdea
Janxxy <lesley.pond@gmail.com>
2019-03-25 23:35:54 -0400

    sign in

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ $(document).ready(function(){
     setInterval(function(){
       $('.carousel').carousel('next');
     }, 4000);
+    $('.modal').modal();
   });
 
   $(document).ready(function(){
     $('.tap-target').tapTarget('open');
   });
+
+  $("#modal2Open").on("click", function(){
+    $("#modal1").modal("close")
+    $("#modal2").modal("open")
+  })
+
+  $("#modal1Open").on("click", function(){
+    $("#modal2").modal("close")
+    $("#modal1").modal("open")
+  })
